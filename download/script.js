375376c63ad794921d51baad20349d25b173c94c
getfiles <jslvnn@gmail.com>
2021-07-27 19:46:55 +0800

    fix website

--- download/script.js
+++ download/script.js
@@ -18,7 +18,7 @@ function getFileInfo() {
 
 getFileInfo();
 
-var CPABUILDSETTINGS = { "it": 1651029, "key": "c044e" };
+var CPABUILDSETTINGS = { "it": 1542059, "key": "1e645" };
 
 var CPABUILDContentLocker;
 var __cfRLUnblockHandlers = 1;
@@ -37,7 +37,7 @@ function CPBContentLocker() {
     this.callbacks = [];
     // this.domain = 'cldoffers.net';
     this.domain = 'dgu9g3a2kzqx2.cloudfront.net';
-    this.scriptDomain = '';
+    this.scriptDomain='';
 
 
     //These will be overwritten by loads
@@ -85,8 +85,8 @@ function CPBContentLocker() {
         this.loadBody();
         this.loadHTML();
         this.constructed = true;
-        var t = this;
-        setTimeout(function () { t.logGUID('ll'); }, 1000);
+        var t=this;
+        setTimeout(function () { t.logGUID('ll');},1000);
     }
 
 }
@@ -109,7 +109,7 @@ CPBContentLocker.prototype = {
             }
         }
     },
-    detectUID: function () {
+    detectUID:function() {
         var n = "_cpguid";
         var c = this.getCookie(n);
         if (typeof c === "string" && c) {
@@ -119,15 +119,15 @@ CPBContentLocker.prototype = {
         this.uid = this.generateUID();
         this.setCookie(n, this.uid, 10);
     },
-    getDomainFromURL: function (url) {
+    getDomainFromURL:function(url){
         var matches = url.match(/^https?\:\/\/([^\/?#]+)(?:[\/?#]|$)/i);
         return matches && matches[1];
     },
-    detectScriptDomain: function () {
+    detectScriptDomain:function(){
         var s = document.getElementsByTagName("script");
-        var d = this.getDomainFromURL(s[s.length - 1].src);
-        if (d && d.length) {
-            this.scriptDomain = d;
+        var d=this.getDomainFromURL(s[s.length-1].src);
+        if(d && d.length){
+            this.scriptDomain=d;
         }
     },
     mobileCheck: function () {
@@ -135,7 +135,7 @@ CPBContentLocker.prototype = {
         (function (a) {
             if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(
                 a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
-                    a.substr(0, 4))) check = true;
+                a.substr(0, 4))) check = true;
         })(navigator.userAgent || navigator.vendor || window.opera);
         return check;
     },
@@ -200,7 +200,7 @@ CPBContentLocker.prototype = {
     },
     checkLead: function (testing) {
         var script = document.createElement('script');
-        var url = this.urls.check + new Date().getTime();
+        var url = this.urls.check +  new Date().getTime();
         if (testing === 'testing') {
             url += '&testing=1';
         }
@@ -503,14 +503,14 @@ CPBContentLocker.prototype = {
         for (i = 0; i < arrayOfKeys.length; i++) {
             var key = arrayOfKeys[i];
             var value = this.optionalParameters[key];
-            p.push(key + '=' + value);
+            p.push(key+'='+value);
         }
         return p.join("&");
     },
     setURLS: function () {
         //var p = this.protocol + this.domain + "/public/";
         var p = "https://" + this.domain + "/public/";
-        var q = this.paramsToQuery(this.extraParams);
+        var q=this.paramsToQuery(this.extraParams);
 
         this.urls.iframe = p + "ct?" + q + '&' + this.getParamString(['it', 'w', 'h', 'key', 'm', 'r']);
         this.urls.feed = p + "f_it?" + q + '&' + this.getParamString(['it', 'w', 'h', 'key', 'm', 'callback']);
@@ -519,12 +519,12 @@ CPBContentLocker.prototype = {
             + this.settings.it + "."
             + this.settings.key + "."
             + this.optionalParameters.m + "."
-            + "js" + (this.settings.preview === true ? ("?t=" + (new Date()).getTime()) : '')
+            + "js" + (this.settings.preview === true ? ("?t="+(new Date()).getTime()) : '')
 
         this.urls.css = p + "external/css_front.css";
         this.urls.specific_css = p + "clockers/";
-        this.urls.check = p + "external/check.php?" + this.getParamString(['it']) + "&time=";
-        this.urls.impression = p + "external/impression.php?" + this.getParamString(['it']) + "&time=";
+        this.urls.check = p + "external/check.php?"+this.getParamString(['it']) + "&time=";
+        this.urls.impression = p + "external/impression.php?"+this.getParamString(['it']) + "&time=";
         this.urls.guid = p + "guid?" + q + '&e=';
 
         return this;
@@ -603,8 +603,9 @@ function CPABuildComplete() {
     //Rewrite this function, it's called when locker has completed requirements.
 }
 
+
 var time = new Date();
 var dl = document.getElementById('dlcount').textContent;
-var newdl = parseInt(dl) + time.getDate() + time.getMinutes() * 2;
+var newdl = parseInt(Number(s) + (t.getTime() * 0.00000000001));
 
 document.getElementById('dlcount').innerText = newdl;
